9d6e76c5244e59b4838b45ed847108a348ea652f
Jadson <jadson.correa@gmail.com>
2015-05-07 09:18:59 -0300

    Update aula.php

--- aula.php
+++ aula.php
@@ -1,9 +1,9 @@
 <?php
 	$texto2 = "Bom Dia";
-	$texto3 = "Meu nome È Jadson Correa de Almeida";
-	$texto1 = "Ol· Mundo"; //aqui atribuo um valor para variavel texto1
-	print("<span style=\"color:#f60; font-size:30px;\">$texto1<br /></span>"); /*evitar usar print pois echo È mais rapido*/
-	/*echo("Ol· pessoal<br />!");*/
-	echo("<span style=\" color:blue; font-size:60px;\">EAE FDPs $texto2<br \></span>");
+	$texto3 = "Meu nome √© Jadson Correa de Almeida";
+	$texto1 = "Ol√° Mundo"; //aqui atribuo um valor para variavel texto1
+	print("<span style=\"color:#f60; font-size:30px;\">$texto1<br /></span>"); /*evitar usar print pois echo √© mais rapido*/
+	/*echo("Ol√° pessoal<br />!");*/
+	echo("<span style=\" color:blue; font-size:60px;\">Teste $texto2<br \></span>");
 	echo ("<span style=\" color:green;font-size:50px;\">$texto3<br /></span>");
 ?>
